c25ddd269303f45baa4b0532c48a3152a708dbc4
hex-willer <manoj.prabhakar272@gmail.com>
2022-04-11 12:12:22 +0530

    last

--- python/dns_lookup.py
+++ python/dns_lookup.py
@@ -1,7 +1,7 @@
 import socket
 
-val = input("Enter the URL : ")
-addr1 = socket.gethostbyname(val)
+val = input("Enter the URL without http and https and slashes : ") 
+addr1 = socket.gethostbyname(val) #this function resolves the ip
 
 print("The dns lookup for url ",val,"is ",addr1) 
 
